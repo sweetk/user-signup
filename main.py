45a14192a7a5e7322e9d7e3119f835f6b783f1f2
KJ Sweet <KJ Sweet>
2017-11-20 20:12:26 -0600

    Added welcome screen

--- main.py
+++ main.py
@@ -8,4 +8,9 @@ app.config['DEBUG'] = True
 def index():
     return render_template('forms.html')
 
+@app.route("/welcome", methods=['POST'])
+def welcome():
+    usrname = request.form['usrname']
+    return render_template('welcome.html', usrname=usrname)
+
 app.run()
