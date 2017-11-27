98d6e4f670d23da628a2f8484acb3bfae44659ac
KJ Sweet <KJ Sweet>
2017-11-26 21:00:31 -0600

    consolidated error code reporting

--- main.py
+++ main.py
@@ -14,15 +14,37 @@ def welcome():
 
     #Pass form input
     usrname = request.form['usrname']
-    #passw = request.form['passw']
-    #pass_verify = request.form['pass_verify']
-    #email = request.form['email']
+    passw = request.form['passw']
+    pass_verify = request.form['pass_verify']
+    email = request.form['email']
 
     #Perform error verification
-    #Just for username first?
+    #For username
     if len(usrname) < 3 or len(usrname) > 20:
         error = "That is not a valid username."
         return redirect("/?error=" + error)
+
+    #for Password
+    if len(passw) < 3 or len(passw) > 20 or " " in passw:
+        error = "That is not a valid password."
+        return redirect("/?error=" + error)
+
+    #for second Password
+    if passw != pass_verify:
+        error = "Passwords don't match."
+        return redirect("/?error=" + error)
+
+    #for email
+    if email:
+        if len(email) < 3 or len(email) > 20 or " " in email:
+            error = "That is not a valid email."
+            return redirect("/?error=" + error)
+        #check special characters
+        if '@' not in email or '.' not in email:
+            error = "That is not a valid email."
+            return redirect("/?error=" + error)
+
+        #too many special characters
 
     #If everything goes right
     return render_template('welcome.html', usrname=usrname)
