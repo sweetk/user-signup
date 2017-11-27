2298df4df47135df8b679e569742efe26b9ccebd
KJ Sweet <KJ Sweet>
2017-11-26 20:26:43 -0600

    Added first error code

--- main.py
+++ main.py
@@ -6,11 +6,25 @@ app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    return render_template('forms.html')
+    encoded_error = request.args.get("error")
+    return render_template('forms.html', error=encoded_error)
 
 @app.route("/welcome", methods=['POST'])
 def welcome():
+
+    #Pass form input
     usrname = request.form['usrname']
+    #passw = request.form['passw']
+    #pass_verify = request.form['pass_verify']
+    #email = request.form['email']
+
+    #Perform error verification
+    #Just for username first?
+    if len(usrname) < 3 or len(usrname) > 20:
+        error = "That is not a valid username."
+        return redirect("/?error=" + error)
+
+    #If everything goes right
     return render_template('welcome.html', usrname=usrname)
 
 app.run()
