7431463007724c133ae5557a8872e2f714cc4cd3
KJ Sweet <KJ Sweet>
2017-11-27 19:00:41 -0600

    Can display multiple errors!

--- main.py
+++ main.py
@@ -4,47 +4,68 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
-    encoded_error = request.args.get("error")
-    return render_template('forms.html', error=encoded_error)
-
-@app.route("/welcome", methods=['POST'])
-def welcome():
-
-    #Pass form input
+def error_verification():
     usrname = request.form['usrname']
     passw = request.form['passw']
     pass_verify = request.form['pass_verify']
     email = request.form['email']
 
-    #Perform error verification
+    error_u = ''
+    error_p = ''
+    error_pv = ''
+    error_e = ''
+
     #For username
     if len(usrname) < 3 or len(usrname) > 20:
-        error = "That is not a valid username."
-        return redirect("/?error=" + error)
+        error_u = "That is not a valid username."
 
     #for Password
     if len(passw) < 3 or len(passw) > 20 or " " in passw:
-        error = "That is not a valid password."
-        return redirect("/?error=" + error)
+        error_p = "That is not a valid password."
 
     #for second Password
     if passw != pass_verify:
-        error = "Passwords don't match."
-        return redirect("/?error=" + error)
+        error_pv = "Passwords don't match."
 
     #for email
     if email:
         if len(email) < 3 or len(email) > 20 or " " in email:
-            error = "That is not a valid email."
-            return redirect("/?error=" + error)
-        #check special characters
-        if '@' not in email or '.' not in email:
-            error = "That is not a valid email."
-            return redirect("/?error=" + error)
+            error_e = "That is not a valid email."
 
-        #too many special characters
+        #check special characters
+        elif '@' not in email or '.' not in email:
+            error_e = "That is not a valid email."
+
+    if error_u or error_p or error_pv or error_e:
+        #return "return redirect('/?error_u=' + error_u + '&error_p=' + error_p + '&error_pv=' + error_pv + '&error_e=' + error_e)"
+        return redirect('/?error_u=' + error_u + '&error_p=' + error_p + '&error_pv=' + error_pv + '&error_e=' + error_e)
+
+    else:
+        return none
+
+@app.route("/")
+def index():
+    #encoded_error = request.args.get("error")
+    #return render_template('forms.html', error=encoded_error)
+
+    encoded_error_u = request.args.get("error_u")
+    encoded_error_p = request.args.get("error_p")
+    encoded_error_pv = request.args.get("error_pv")
+    encoded_error_e = request.args.get("error_e")
+
+    return render_template('forms.html', error_u=encoded_error_u, error_p=encoded_error_p, error_pv=encoded_error_pv, error_e=encoded_error_e)
+
+@app.route("/welcome", methods=['POST'])
+def welcome():
+
+    #error_verification() #NOT WORKING
+
+    is_error = error_verification()
+    if is_error:
+        return is_error
+
+    #Pass form input
+    usrname = request.form['usrname']
 
     #If everything goes right
     return render_template('welcome.html', usrname=usrname)
